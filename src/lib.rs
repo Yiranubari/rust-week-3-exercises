133acbb0285fe7a14f7a1c9cb28d792fb8473a71
Yiranubari <yiranubari4@gmail.com>
2026-06-12 12:41:30 +0100

    implement OutPoint struct methods: new, to_bytes, and from_bytes

--- src/lib.rs
+++ src/lib.rs
@@ -116,18 +116,30 @@ pub struct OutPoint {
 impl OutPoint {
     pub fn new(txid: [u8; 32], vout: u32) -> Self {
         // TODO: Create an OutPoint from raw txid bytes and output index
-        todo!()
+        Self {
+            txid: Txid(txid),
+            vout,
+        }
     }
 
     pub fn to_bytes(&self) -> Vec<u8> {
         // TODO: Serialize as: txid (32 bytes) + vout (4 bytes, little-endian)
-        todo!()
+        let mut bytes = Vec::new();
+        bytes.extend_from_slice(&self.txid.0);
+        bytes.extend_from_slice(&self.vout.to_le_bytes());
+        bytes
     }
 
     pub fn from_bytes(bytes: &[u8]) -> Result<(Self, usize), BitcoinError> {
         // TODO: Deserialize 36 bytes: txid[0..32], vout[32..36]
         // Return error if insufficient bytes
-            todo!()
+        if bytes.len() < 36 {
+            return Err(BitcoinError::InsufficientBytes);
+        }
+        let mut txid_bytes = [0u8; 32];
+        txid_bytes.copy_from_slice(&bytes[0..32]);
+        let vout = u32::from_le_bytes([bytes[32], bytes[33], bytes[34], bytes[35]]);
+        Ok((OutPoint { txid: Txid(txid_bytes), vout }, 36))
     }
 }
 
